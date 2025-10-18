53293a5bd3f55f3bbb5715621ca8f20082e647ee
Eduardo <20dubu06@gmail.com>
2025-10-17 22:01:19 -0300

    Rota tarefas

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template
-
+from tarefa import buscar_tarefas
 app = Flask(__name__)
 
 
@@ -7,9 +7,14 @@ app = Flask(__name__)
 def login(usuario):
     return render_template('index.html', usuario=usuario)
 
-@app.route('/')
+@app.route('/home')
 def home():
     return render_template('home.html')
+
+@app.route('/home/tarefas')
+def get_tarefa():
+    tarefas = buscar_tarefas()
+    return tarefas
 
 if __name__ == '__main__':
     app.run(debug=True)
